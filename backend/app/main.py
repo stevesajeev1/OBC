7d0f9cb13f5de48693960846efb4771bb46b868c
Steve Sajeev <stevesajeev123@gmail.com>
2025-10-09 13:56:10 -0400

    Remove prisma

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,11 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import greeting
 from .util.origin import get_allowed_origin
-from .util.prisma import Prisma
 
 
-@asynccontextmanager
-async def lifespan(_):
-    prisma = Prisma()
-    await prisma.connect()
-    yield
-    await prisma.disconnect()
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 allowed_origin = get_allowed_origin()
 app.add_middleware(
