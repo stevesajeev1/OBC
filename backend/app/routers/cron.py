f6a294e7a35fde0a07679b4ee0baf8ca351628e6
Steve Sajeev <stevesajeev123@gmail.com>
2025-10-13 13:45:14 -0400

    Update requirements.txt

--- backend/app/routers/cron.py
+++ backend/app/routers/cron.py
@@ -2,7 +2,7 @@ import os
 from typing import Annotated
 
 from dotenv import load_dotenv
-from fastapi import APIRouter, Header, HTTPException
+from fastapi import APIRouter, Header, HTTPException, status
 
 load_dotenv()
 
@@ -14,7 +14,7 @@ router = APIRouter(prefix="/cron", tags=["cron"])
 @router.get("/")
 async def scrape(Authorization: Annotated[str, Header()]):
     if Authorization != f"Bearer {CRON_SECRET}":
-        raise HTTPException(status_code=401, detail="Unauthorized")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
 
     # Do logic here
 
