1655fa54b995424d27ff941b548cd1d65db94b5d
schinta-prog <schinta@ufl.edu>
2025-10-28 23:15:43 -0400

    Landing Page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,5 @@
 ﻿import { createRouter, createWebHistory } from 'vue-router';
-// 1. Import the new component for the default landing page
 import LandingView from '../views/LandingView.vue';
-// 2. Import HomeView, but it will now be used for the internships route
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
@@ -10,16 +8,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // ⭐ Use the new LandingView for the root path
       component: LandingView
     },
     {
-      path: '/internships', // ⭐ This is the new path for the internships button
+      path: '/internships',
       name: 'internships',
-      // ⭐ HomeView (your old landing page) now shows up here
       component: HomeView
     },
-    // The original '/about' route (it's often good to keep 'about' if it exists)
     {
       path: '/about',
       name: 'about',
