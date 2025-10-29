13c1b46f231d1526e4e72a975f08a84168997a48
schinta-prog <schinta@ufl.edu>
2025-10-28 23:33:39 -0400

    m

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,5 @@
 ﻿import { createRouter, createWebHistory } from 'vue-router';
-
 import LandingView from '../views/LandingView.vue';
-
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
