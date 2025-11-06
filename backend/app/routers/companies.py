411acf5b0e38568a53f09ece6ba2a1dcf5e99eae
Steve Sajeev <stevesajeev123@gmail.com>
2025-11-06 08:43:23 -0500

    Extract pagination into separate class

--- backend/app/routers/companies.py
+++ backend/app/routers/companies.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, status
 
-from ..models.companies import CompaniesResponse
+from ..models.api import PaginatedResponse
+from ..models.companies import Company
 from ..util.companies import get_companies_from_db
 
 # --- router ---
@@ -8,25 +9,9 @@ router = APIRouter(prefix="/companies", tags=["Companies"])
 
 
 # --- api endpoints ---
-@router.get("/", response_model=CompaniesResponse, status_code=status.HTTP_200_OK)
-def get_listings(request: Request, page: int = 0, pageSize: int = 100):
+@router.get(
+    "/", response_model=PaginatedResponse[Company], status_code=status.HTTP_200_OK
+)
+def get_companies(request: Request, page: int = 0, pageSize: int = 100):
     companies = get_companies_from_db()
-    count = len(companies)
-
-    start = page * pageSize
-    end = start + pageSize
-
-    base_url = str(request.url).split("?")[0]
-    next_url, prev_url = None, None
-    if end < count:
-        next_url = f"{base_url}?page={page + 1}&pageSize={pageSize}"
-    if page > 0:
-        prev_url = f"{base_url}?page={page - 1}&pageSize={pageSize}"
-
-    response = {
-        "count": count,
-        "next": next_url,
-        "previous": prev_url,
-        "results": companies[start:end],
-    }
-    return CompaniesResponse.model_validate(response)
+    return PaginatedResponse.paginate(companies, page, pageSize, str(request.url))
