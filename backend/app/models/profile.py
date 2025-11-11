3f5c07824ed19f71045f2d9cc7ca6d0d51423b28
sriramyy <131324693+sriramyy@users.noreply.github.com>
2025-11-11 14:49:39 -0500

    formatting

--- backend/app/models/profile.py
+++ backend/app/models/profile.py
@@ -1,26 +1,31 @@
 from pydantic import BaseModel
 
+
 class Internship(BaseModel):
     """structure for past internship"""
+
     company: str
     role: str
-    time_period: list[str] # ex. Summer 2025
+    time_period: list[str]  # ex. Summer 2025
+
 
 class Profile(BaseModel):
     """fields that are allowed to be updated by user"""
+
     full_name: str
     major: str
-    grad_year: int # Class of 2028
+    grad_year: int  # Class of 2028
     linkedin_link: str
     bio: str | None = None
     prev_internships: list[Internship] | None = None
 
+
 class ProfileUpdate(BaseModel):
     """fields that can be updated"""
+
     full_name: str | None = None
     major: str | None = None
     grad_year: int | None = None
     linkedin_link: str | None = None
     bio: str | None = None
     prev_internships: list[Internship] | None = None
-
