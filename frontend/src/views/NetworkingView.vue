ae4d5b9aa71724d173a3441905aad309d1560f3b
sriramyy <131324693+sriramyy@users.noreply.github.com>
2025-11-13 09:59:59 -0500

    Start working on networking page

---
+++ frontend/src/views/NetworkingView.vue
@@ -0,0 +1,670 @@
+<template>
+  	<div :class="$style.networkingPage">
+    		<div :class="$style.people">
+      			<div :class="$style.bimBob">
+        				<div :class="$style.bimBobChild" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt">Interned at: </div>
+        				<div :class="$style.contact">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob2">
+        				<div :class="$style.bimBobInner" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt">Interned at: </div>
+        				<div :class="$style.contact">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob4">
+        				<div :class="$style.rectangleDiv" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt">Interned at: </div>
+        				<div :class="$style.contact">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob6">
+        				<div :class="$style.bimBobChild" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt3">Interned at: </div>
+        				<div :class="$style.contact3">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob8">
+        				<div :class="$style.bimBobInner" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt3">Interned at: </div>
+        				<div :class="$style.contact3">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob10">
+        				<div :class="$style.rectangleDiv" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt3">Interned at: </div>
+        				<div :class="$style.contact3">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob12">
+        				<div :class="$style.bimBobChild" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt3">Interned at: </div>
+        				<div :class="$style.contact3">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob14">
+        				<div :class="$style.bimBobInner" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt3">Interned at: </div>
+        				<div :class="$style.contact3">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+      			<div :class="$style.bimBob16">
+        				<div :class="$style.rectangleDiv" />
+        				<div :class="$style.networkingPageBimBob">Bim Bob</div>
+        				<div :class="$style.softwareEngineer">Software Engineer</div>
+        				<div :class="$style.internedAt3">Interned at: </div>
+        				<div :class="$style.contact3">
+          					<div :class="$style.text">
+            						<div :class="$style.button">Contact</div>
+          					</div>
+        				</div>
+        				<img :class="$style.phIcon" alt="" />
+        				<img :class="$style.bimBobItem" alt="" />
+      			</div>
+    		</div>
+    		<div :class="$style.filterPositions">
+      			<div :class="$style.combobox">
+        				<div :class="$style.text9">
+          					<div :class="$style.networkingPageFilterPositions">Filter Positions</div>
+        				</div>
+        				<img :class="$style.icon" alt="" />
+      			</div>
+      			<div :class="$style.dropdown">
+        				<div :class="$style.divider" />
+        				<div :class="$style.wrapper">
+          					<div :class="$style.networkingPageWrapper">
+            						<div :class="$style.text">
+              							<div :class="$style.softwareEngineering">Software Engineering</div>
+            						</div>
+          					</div>
+          					<div :class="$style.wrapper2">
+            						<div :class="$style.text">
+              							<div :class="$style.productManagement">Product Management</div>
+            						</div>
+          					</div>
+          					<div :class="$style.wrapper2">
+            						<div :class="$style.text">
+              							<div :class="$style.productManagement">Data Science / AI / ML</div>
+            						</div>
+          					</div>
+          					<div :class="$style.wrapper2">
+            						<div :class="$style.text">
+              							<div :class="$style.productManagement">Quantitative Finance</div>
+            						</div>
+          					</div>
+          					<div :class="$style.wrapper2">
+            						<div :class="$style.text">
+              							<div :class="$style.hardwareEngineering">Hardware Engineering</div>
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+    		</div>
+    		<div :class="$style.searchJobs">
+      			<img :class="$style.searchIcon" alt="" />
+      			<div :class="$style.text">
+        				<div :class="$style.networkingPageFilterPositions">Companies that a gator may have interned at!</div>
+      			</div>
+    		</div>
+    		<div :class="$style.orangeAndBlueContainer">
+      			<span :class="$style.orange">Orange</span>
+      			<span> and </span>
+      			<span :class="$style.blue">Blue</span>
+      			<span> Collar</span>
+    		</div>
+    		<div :class="$style.navbarV03">
+      			<div :class="$style.options">
+        				<img :class="$style.zoomedTransparentGator1" alt="" />
+        				<div :class="$style.homeParent">
+          					<div :class="$style.home">Home</div>
+          					<div :class="$style.networking">Networking</div>
+          					<div :class="$style.jobs">Jobs</div>
+          					<div :class="$style.button">About</div>
+          					<div :class="$style.loginHovered">
+            						<div :class="$style.loginHoveredChild" />
+            						<div :class="$style.login">Login</div>
+          					</div>
+          					<div :class="$style.networkingPageLogin" @click="onLoginContainerClick">
+            						<div :class="$style.loginChild" />
+            						<div :class="$style.login">Login</div>
+          					</div>
+          					<div :class="$style.signUp" @click="onLoginContainerClick">
+            						<div :class="$style.signUpChild" />
+            						<div :class="$style.joinNow">Join Now</div>
+          					</div>
+        				</div>
+      			</div>
+    		</div>
+  	</div>
+</template>
+<script setup lang="ts">
+  	
+  	
+  	function onLoginContainerClick() {
+    		// Add your code here
+  	}</script>
+<style  module>.networkingPage {
+  	width: 100%;
+  	height: 1173px;
+  	position: relative;
+  	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  	background: linear-gradient(180deg, #5a7caf 74.52%, #b5b5b5);
+  	overflow: hidden;
+  	text-align: left;
+  	font-size: 20px;
+  	color: #fff;
+  	font-family: 'Irish Grover';
+}
+  	.people {
+    		position: absolute;
+    		top: 584px;
+    		left: 176px;
+    		width: 1066px;
+    		height: 684px;
+  	}
+  	.bimBob {
+    		position: absolute;
+    		top: 0px;
+    		left: 0px;
+    		width: 335px;
+    		height: 188px;
+    		font-family: 'Inria Sans';
+  	}
+  	.bimBobChild {
+    		position: absolute;
+    		top: 0px;
+    		left: 0px;
+    		border-radius: 6px;
+    		background: linear-gradient(180deg, #5a7caf 74.52%, #b5b5b5);
+    		border: 1px solid #d4862d;
+    		box-sizing: border-box;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.networkingPageBimBob {
+    		position: absolute;
+    		top: 14px;
+    		left: 113px;
+    		font-size: 32px;
+  	}
+  	.softwareEngineer {
+    		position: absolute;
+    		top: 50px;
+    		left: 113px;
+    		font-size: 16px;
+  	}
+  	.internedAt {
+    		position: absolute;
+    		top: 84px;
+    		left: 29px;
+  	}
+  	.contact {
+    		position: absolute;
+    		top: 119px;
+    		left: 68px;
+    		border-radius: 6px;
+    		background-color: #d4862d;
+    		border: 1px solid #000;
+    		box-sizing: border-box;
+    		width: 209px;
+    		height: 40px;
+    		display: flex;
+    		flex-direction: column;
+    		align-items: center;
+    		justify-content: center;
+    		padding: 8px 16px;
+    		min-height: 40px;
+    		max-height: 40px;
+    		color: #fafafa;
+    		font-family: 'Irish Grover';
+  	}
+  	.text {
+    		display: flex;
+    		align-items: center;
+    		justify-content: center;
+  	}
+  	.button {
+    		position: relative;
+    		text-shadow: 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000;
+  	}
+  	.phIcon {
+    		position: absolute;
+    		top: 80px;
+    		left: 139px;
+    		border-radius: 50%;
+    		width: 30px;
+    		height: 30px;
+    		object-fit: cover;
+  	}
+  	.bimBobItem {
+    		position: absolute;
+    		top: 12px;
+    		left: 32px;
+    		border-radius: 50%;
+    		width: 60px;
+    		height: 60px;
+    		object-fit: cover;
+  	}
+  	.bimBob2 {
+    		position: absolute;
+    		top: 0px;
+    		left: 366px;
+    		width: 335px;
+    		height: 188px;
+    		font-family: 'Inria Sans';
+  	}
+  	.bimBobInner {
+    		position: absolute;
+    		top: 0px;
+    		left: 0px;
+    		border-radius: 6px;
+    		background: linear-gradient(180deg, rgba(0, 0, 0, 0.5), rgba(14, 26, 61, 0.5));
+    		border: 1px solid #d4862d;
+    		box-sizing: border-box;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.bimBob4 {
+    		position: absolute;
+    		top: 3px;
+    		left: 731px;
+    		width: 335px;
+    		height: 188px;
+    		font-family: 'Inria Sans';
+  	}
+  	.rectangleDiv {
+    		position: absolute;
+    		top: 0px;
+    		left: 0px;
+    		border-radius: 6px;
+    		background: linear-gradient(180deg, rgba(244, 39, 39, 0.5), rgba(14, 26, 61, 0.5));
+    		border: 1px solid #d4862d;
+    		box-sizing: border-box;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.bimBob6 {
+    		position: absolute;
+    		top: 247px;
+    		left: 0px;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.internedAt3 {
+    		position: absolute;
+    		top: 84px;
+    		left: 29px;
+    		font-family: 'Inria Sans';
+  	}
+  	.contact3 {
+    		position: absolute;
+    		top: 119px;
+    		left: 68px;
+    		border-radius: 6px;
+    		background-color: #d4862d;
+    		border: 1px solid #000;
+    		box-sizing: border-box;
+    		width: 209px;
+    		height: 40px;
+    		display: flex;
+    		flex-direction: column;
+    		align-items: center;
+    		justify-content: center;
+    		padding: 8px 16px;
+    		min-height: 40px;
+    		max-height: 40px;
+    		color: #fafafa;
+  	}
+  	.bimBob8 {
+    		position: absolute;
+    		top: 247px;
+    		left: 366px;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.bimBob10 {
+    		position: absolute;
+    		top: 250px;
+    		left: 731px;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.bimBob12 {
+    		position: absolute;
+    		top: 493px;
+    		left: 0px;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.bimBob14 {
+    		position: absolute;
+    		top: 493px;
+    		left: 366px;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.bimBob16 {
+    		position: absolute;
+    		top: 496px;
+    		left: 731px;
+    		width: 335px;
+    		height: 188px;
+  	}
+  	.filterPositions {
+    		position: absolute;
+    		top: 488px;
+    		left: 176px;
+    		display: flex;
+    		flex-direction: column;
+    		align-items: flex-start;
+    		gap: 4px;
+    		font-size: 14px;
+  	}
+  	.combobox {
+    		width: 150px;
+    		height: 40px;
+    		border-radius: 6px;
+    		background-color: rgba(255, 255, 255, 0);
+    		border: 1px solid #d4862d;
+    		box-sizing: border-box;
+    		display: flex;
+    		align-items: center;
+    		padding: 8px 16px;
+    		min-height: 40px;
+    		max-height: 40px;
+  	}
+  	.text9 {
+    		width: 93px;
+    		display: flex;
+    		align-items: center;
+  	}
+  	.networkingPageFilterPositions {
+    		position: relative;
+    		opacity: 0.5;
+  	}
+  	.icon {
+    		height: 16px;
+    		width: 24px;
+    		opacity: 0.5;
+  	}
+  	.dropdown {
+    		box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.1), 0px 2px 4px -1px rgba(0, 0, 0, 0.06);
+    		border-radius: 6px;
+    		background-color: rgba(0, 0, 0, 0);
+    		border: 1px solid #d4862d;
+    		display: none;
+    		flex-direction: column;
+    		align-items: flex-start;
+  	}
+  	.divider {
+    		align-self: stretch;
+    		height: 0px;
+    		display: flex;
+    		flex-direction: column;
+    		align-items: flex-start;
+  	}
+  	.wrapper {
+    		align-self: stretch;
+    		display: flex;
+    		flex-direction: column;
+    		align-items: flex-start;
+    		padding: 4px;
+  	}
+  	.networkingPageWrapper {
+    		width: 190px;
+    		border-radius: 2px;
+    		background-color: rgba(255, 136, 0, 0.69);
+    		display: flex;
+    		align-items: center;
+    		padding: 6px 8px;
+    		box-sizing: border-box;
+  	}
+  	.softwareEngineering {
+    		position: relative;
+    		text-shadow: 0.3px 0 0 #000, 0 0.3px 0 #000, -0.3px 0 0 #000, 0 -0.3px 0 #000;
+  	}
+  	.wrapper2 {
+    		width: 190px;
+    		border-radius: 2px;
+    		display: flex;
+    		align-items: center;
+    		padding: 6px 8px;
+    		box-sizing: border-box;
+  	}
+  	.productManagement {
+    		position: relative;
+  	}
+  	.hardwareEngineering {
+    		width: 147px;
+    		position: relative;
+    		display: inline-block;
+    		flex-shrink: 0;
+  	}
+  	.searchJobs {
+    		position: absolute;
+    		top: 360px;
+    		left: 177px;
+    		border-radius: 6px;
+    		border: 1px solid #d4862d;
+    		box-sizing: border-box;
+    		width: 1065px;
+    		height: 72px;
+    		display: flex;
+    		align-items: center;
+    		padding: 4px 12px 4px 17px;
+    		gap: 17px;
+    		font-size: 24px;
+  	}
+  	.searchIcon {
+    		height: 20px;
+    		width: 20px;
+    		opacity: 0.5;
+  	}
+  	.orangeAndBlueContainer {
+    		position: absolute;
+    		top: 213px;
+    		left: 272px;
+    		font-size: 80px;
+    		display: inline-block;
+    		width: 863px;
+    		height: 116px;
+    		filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    		text-shadow: 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000;
+  	}
+  	.orange {
+    		color: #d4862d;
+  	}
+  	.blue {
+    		color: #1e90ff;
+  	}
+  	.navbarV03 {
+    		position: absolute;
+    		top: 9px;
+    		left: 8px;
+    		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    		background: linear-gradient(90deg, #5a7caf 0.01%);
+    		border: 1px solid rgba(0, 0, 0, 0.1);
+    		box-sizing: border-box;
+    		width: 1425px;
+    		height: 90px;
+    		overflow: hidden;
+    		font-size: 36px;
+  	}
+  	.options {
+    		position: absolute;
+    		top: 0px;
+    		left: 38px;
+    		width: 1375px;
+    		height: 90px;
+    		display: flex;
+    		align-items: center;
+    		justify-content: space-between;
+    		gap: 20px;
+  	}
+  	.zoomedTransparentGator1 {
+    		width: 89px;
+    		position: relative;
+    		max-height: 100%;
+    		object-fit: cover;
+  	}
+  	.homeParent {
+    		display: flex;
+    		align-items: center;
+    		gap: 32px;
+  	}
+  	.home {
+    		height: 44px;
+    		width: 93px;
+    		position: relative;
+    		display: inline-block;
+    		flex-shrink: 0;
+    		text-shadow: 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000;
+  	}
+  	.networking {
+    		height: 44px;
+    		width: 184px;
+    		position: relative;
+    		text-decoration: underline;
+    		display: inline-block;
+    		flex-shrink: 0;
+    		text-shadow: 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000;
+  	}
+  	.jobs {
+    		height: 44px;
+    		width: 75px;
+    		position: relative;
+    		display: inline-block;
+    		flex-shrink: 0;
+    		text-shadow: 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000;
+  	}
+  	.loginHovered {
+    		height: 52px;
+    		width: 111px;
+    		position: relative;
+    		display: none;
+  	}
+  	.loginHoveredChild {
+    		position: absolute;
+    		top: 0px;
+    		left: 0px;
+    		border-radius: 8px;
+    		background-color: #d4862d;
+    		border: 1px solid #000;
+    		box-sizing: border-box;
+    		width: 111px;
+    		height: 52px;
+    		opacity: 0.5;
+  	}
+  	.login {
+    		position: absolute;
+    		top: 3px;
+    		left: 10px;
+    		display: inline-block;
+    		width: 94px;
+    		height: 44px;
+    		text-shadow: 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000;
+  	}
+  	.networkingPageLogin {
+    		height: 52px;
+    		width: 111px;
+    		position: relative;
+    		cursor: pointer;
+  	}
+  	.loginChild {
+    		position: absolute;
+    		top: 0px;
+    		left: 0px;
+    		border-radius: 8px;
+    		background-color: #d4862d;
+    		border: 1px solid #000;
+    		box-sizing: border-box;
+    		width: 111px;
+    		height: 52px;
+    		opacity: 0.1;
+  	}
+  	.signUp {
+    		height: 52px;
+    		width: 166px;
+    		position: relative;
+    		cursor: pointer;
+  	}
+  	.signUpChild {
+    		position: absolute;
+    		top: 0px;
+    		left: 0px;
+    		border-radius: 8px;
+    		background-color: #d4862d;
+    		border: 1px solid #000;
+    		box-sizing: border-box;
+    		width: 166px;
+    		height: 52px;
+    		opacity: 0.1;
+  	}
+  	.joinNow {
+    		position: absolute;
+    		top: 3px;
+    		left: 12px;
+    		display: inline-block;
+    		width: 149px;
+    		height: 44px;
+    		text-shadow: 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000, 0 -1px 0 #000;
+  	}
+
+</style>
