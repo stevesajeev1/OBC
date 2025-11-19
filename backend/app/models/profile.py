ef64de8fb344c14ecce34cabec911d1883bfc804
sriramyy <131324693+sriramyy@users.noreply.github.com>
2025-11-18 19:46:33 -0500

    Updated Profile files with suggestions

--- backend/app/models/profile.py
+++ backend/app/models/profile.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Any
 
 
 class Internship(BaseModel):
@@ -17,8 +18,31 @@ class Profile(BaseModel):
     grad_year: int  # Class of 2028
     linkedin_link: str
     bio: str | None = None
-    prev_internships: list[Internship] | None = None
+    image_url: str | None = None
+    prev_internships: list[Internship] = []
 
+
+    @classmethod
+    def from_db(cls, profile_row: dict[str, Any], internship_rows: list[dict[str, Any]]):
+        """classmethod to create profile instance from db row"""
+        internships = []
+        if internship_rows:
+            for row in internship_rows:
+                internships.append(Internship(
+                    company = row['company'],
+                    role = row['role'],
+                    time_period = row['time_period']
+                ))
+
+        return cls(
+            full_name=profile_row['full_name'],
+            major=profile_row['major'],
+            grad_year=profile_row['grad_year'],
+            linkedin_link=profile_row['linkedin_url'], 
+            bio=profile_row['bio'],
+            image_url=profile_row.get('image_url'),
+            prev_internships=internships
+        )
 
 class ProfileUpdate(BaseModel):
     """fields that can be updated"""
